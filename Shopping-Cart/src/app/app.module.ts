6194c4c19076e1a5359d71d1724842f78845cb39
1YanDevo <wadeyannick@gmail.com>
2022-08-29 11:33:19 -0500

    adding bootstrap & component selectors to app-component

--- Shopping-Cart/src/app/app.module.ts
+++ Shopping-Cart/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { ShopPageComponent } from './components/shop-page/shop-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent,
+    ShopPageComponent
   ],
   imports: [
     BrowserModule
